2a9e7d8990b3a778825b3eb4d38e32831c9fed92
olkhovnikita <65450206+olkhovnikita@users.noreply.github.com>
2021-03-01 05:42:14 +0300

    Update postcard-ready.js

--- components/postcard-ready/postcard-ready.js
+++ components/postcard-ready/postcard-ready.js
@@ -7,11 +7,7 @@ var postcardReady = Vue.component('postcard-ready', {
         <p class='form-text pic-form-text'>Ты можешь скачать открытку и отправить её коллеге</p>
         <div class='info-btns add-info-btns'>
             <button type='button' class='next-btn' @click="changePage('add-effect')">Назад</button>
-<<<<<<< HEAD
-            <a href='#' download class='next-btn load'>Скачать</a>
-=======
-            <a href='#' id="download" class='next-btn load'>Скачать</a>
->>>>>>> 8dcac6a731eeb9eb9950ab25acfb130c4f34fe58
+            <a href='finalImg' download class='next-btn load'>Скачать</a>
             <button type='button' class='next-btn' @click="changePage('send-postcard')">Далее</button>
     </div>
     </div>
