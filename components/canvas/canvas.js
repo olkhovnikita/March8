15b6d44f0dcae460be0fa652904e9724fa2f934e
noobusdeer <hello@noobusdeer.dev>
2021-03-02 09:15:14 +0300

    changeCanvCrop

--- components/canvas/canvas.js
+++ components/canvas/canvas.js
@@ -24,7 +24,7 @@ var myCanvas = Vue.component('my-canvas', {
             context.clearRect(0, 0, canvas.width, canvas.height);
         },
         saveCanvas: function () {
-            function cropPlusExport(img,cropX,cropY,cropWidth,cropHeight){
+            function cropPlusExport(img,cropX,cropY,cropWidth,cropHeight, canvWidth){
                 // create a temporary canvas sized to the cropped size
                 var canvas1=document.createElement('canvas');
                 var ctx1=canvas1.getContext('2d');
@@ -32,7 +32,7 @@ var myCanvas = Vue.component('my-canvas', {
                 canvas1.height=cropHeight;
                 // use the extended from of drawImage to draw the
                 // cropped area to the temp canvas
-                ctx1.drawImage(img,cropX,cropY,cropWidth,cropHeight,0,0,cropWidth,cropHeight);
+                ctx1.drawImage(img,cropX,cropY,cropWidth,cropHeight,cropWidth/2 - canvWidth/2,0,cropWidth,cropHeight);
                 // return the .toDataURL of the temp canvas
                 return(canvas1.toDataURL());
               }
@@ -46,7 +46,8 @@ var myCanvas = Vue.component('my-canvas', {
                 img.src = canvas.toDataURL();
                 self = this;
                 img.onload = function() {
-                    var croppedURL=cropPlusExport(img, 0, canvas.height/2 - (9 * canvas.width/ 16)/2, canvas.width,  9 * canvas.width/ 16);
+                    //var croppedURL=cropPlusExport(img, 0, canvas.height/2 - (9 * canvas.width/ 16)/2, canvas.width,  9 * canvas.width/ 16); croppX
+                    var croppedURL=cropPlusExport(img, canvas.width/2 - (canvas.height * 16/9) /2 , 0, (canvas.height * 16)/9, canvas.height, canvas.width);
                     //var cropImg=new Image();
                     //cropImg.src=croppedURL;
     
